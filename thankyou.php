0a32eec7a46e50b390fa34845b38320c6b7647aa
Grinalasw <frontendasw@gmail.com>
2025-11-25 14:17:28 +0530

    thankyou page

---
+++ thankyou.php
@@ -0,0 +1,74 @@
+<?php include 'Template/header.php'; ?>
+<?php include 'Template/header-main.php'; ?>
+
+<section class="thankyou-section section-gap double-gap">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="main-title">Thank you for your interest in the NMIMS DAT Programs!</h2>
+                <p class="sub-para">
+                    You should have received your login details via SMS or email. To complete your Application, simply
+                    follow below steps
+                </p>
+
+                <div class="admission-section">
+                    <div class="row gx-0">
+                        <div class="col-12">
+                            <div class="admission-wrap" data-aos="fade-up">
+                                <div class="top-content">
+                                    <h6 class="sm-title">Steps to Apply</h6>
+                                    <div class="steps" data-aos="fade-up">
+                                        <div class="item">
+                                            <span class="step d-block"><span>Step 1</span></span>
+                                            <div class="text">
+                                                <p>Register online and pay the application fee</p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <span class="step d-block"><span>Step 2</span></span>
+                                            <div class="text">
+                                                <p>Appear for Entrance Exam & personal Interview</p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <span class="step d-block"><span>Step 3</span></span>
+                                            <div class="text">
+                                                <p>Check your name in the Merit List</p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <span class="step d-block"><span>Step 4</span></span>
+                                            <div class="text">
+                                                <p>Complete the admission form with required documents</p>
+                                            </div>
+                                        </div>
+                                        <div class="item">
+                                            <span class="step d-block"><span>Step 5</span></span>
+                                            <div class="text">
+                                                <p>Pay the admission fee & complete the process</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="text-center">
+                    <a href="#" title="Back to Home" class="btn btn-primary">Back To Home <span class="icon"><i
+                                class="n-icon" data-icon="i-right-arrow"></i></span></a>
+                </div>
+                <p class="thankyou-bottom">
+                    For any assistance, feel free to contact us at
+                    <a href="tel:1800 102 5138">1800 102 5138</a>
+                    or email us at
+                    <a href="mailto:enquiry@nmims.edu">enquiry@nmims.edu</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'Template/footer-main.php'; ?>
+<?php include 'Template/footer.php'; ?>
